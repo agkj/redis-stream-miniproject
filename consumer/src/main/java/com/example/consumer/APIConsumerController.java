d4edb57dbf692707b88eb601b9e17b0f6edf08ef
agkj <alger.goh99@gmail.com>
2024-02-08 21:42:11 +0800

    Renamed API controller class

--- consumer/src/main/java/com/example/consumer/APIConsumerController.java
+++ consumer/src/main/java/com/example/consumer/APIConsumerController.java
@@ -1,9 +1,7 @@
-package com.example.consumer.Controllers;
+package com.example.consumer;
 
-import com.example.consumer.Services.Consumer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.stream.MapRecord;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,23 +12,25 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/consumer")
-//consumer uses port 8081
+//consumerService uses port 8081
 public class APIConsumerController {
 
-    private final Consumer consumer;
+    private final ConsumerService consumerService;
 
+    //Retrieves singular message
     @GetMapping(path = "/retrieve-users")
     public List<MapRecord<String ,Object,Object>> retrieveUsers(){
-        return consumer.retrieveUsers();
+        return consumerService.retrieveUsers();
         //return "list of users";
     }
-    //TO EDIT THIS SHIT
+    //Retrieve entire stream of gates
     @GetMapping(path = "/retrieve-gates")
     public List<MapRecord<String ,Object,Object>> retrieveGates(){
-        return consumer.retrieveGates();
+        return consumerService.retrieveGates();
         //return "list of users";
     }
 
+    //Retrieves JSON data
     @GetMapping("/jsondata")
     public String getJSONData() {
         // Retrieve JSON data from the link
