5290706ab3be706987f60fe60e61bcfec71b031b
agkj <alger.goh99@gmail.com>
2024-02-08 21:37:28 +0800

    Added additional group keys

--- consumer/src/main/java/com/example/consumer/SharedKeysEnum.java
+++ consumer/src/main/java/com/example/consumer/SharedKeysEnum.java
@@ -4,6 +4,7 @@ public enum SharedKeysEnum {
     USER_STREAM_KEY,
     USER_GROUP_KEY,
     GATE_STREAM_KEY,
-    GATE_GROUP_KEY;
+    GATE_GROUP_KEY_A,
+    GATE_GROUP_KEY_B;
 
 }
--- producer/src/main/java/com/example/producer/SharedKeysEnum.java
+++ producer/src/main/java/com/example/producer/SharedKeysEnum.java
@@ -5,5 +5,6 @@ public enum SharedKeysEnum {
     USER_STREAM_KEY,
     USER_GROUP_KEY,
     GATE_STREAM_KEY,
-    GATE_GROUP_KEY;
+    GATE_GROUP_KEY_A,
+    GATE_GROUP_KEY_B;
 }
