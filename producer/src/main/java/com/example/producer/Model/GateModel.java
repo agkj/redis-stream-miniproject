ec2f0f05ed3e9441e8f1f6ed8c314c6b770fc4cd
agkj <alger.goh99@gmail.com>
2024-02-08 21:39:20 +0800

    Added aditional object variables

--- producer/src/main/java/com/example/producer/Model/GateModel.java
+++ producer/src/main/java/com/example/producer/Model/GateModel.java
@@ -10,8 +10,13 @@ import lombok.extern.slf4j.Slf4j;
 
 public class GateModel {
 
-    private int gateNumber;
-    private boolean gateStatus;
+    private int gateNumber; //gets gate number
+    private String gateGroup; // gate group -> need a key
+    private int timestamp; // stream entry time
+    private boolean gateStatus; //gets gate current status, pass or fail
+
+
+    //TODO: Implement checkbox method, when checked, disable autodeletion for checked streams
     private boolean gateHold;
 
 }
