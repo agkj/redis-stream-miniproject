4a20bc2edc77cb8f5347d4a38034e2c399ff676c
agkj <alger.goh99@gmail.com>
2024-02-15 20:20:41 +0800

    Repackaged ConsumerRepository to new package and added new interface functions for retrieval of consumer information in mySQL database

---
+++ consumer/src/main/java/com/example/consumer/Repository/ConsumerRepository.java
@@ -0,0 +1,19 @@
+package com.example.consumer.Repository;
+
+import com.example.consumer.DTO.LoginDTO;
+import com.example.consumer.Model.ConsumerModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@EnableJpaRepositories
+public interface ConsumerRepository extends JpaRepository<ConsumerModel, Integer> {
+
+
+    Optional<ConsumerModel> findByUsernameAndPassword(String username, String password);
+
+    ConsumerModel findByUsername(String username);
+}
