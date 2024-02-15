385a6f1ad45cda63edd4715d3abfa2597bb8c2a6
agkj <alger.goh99@gmail.com>
2024-02-15 20:16:59 +0800

    Added ConsumerApplication to test out consumer creation and add to mysql database

--- consumer/src/test/java/com/example/consumer/ConsumerServiceApplicationTests.java
+++ consumer/src/test/java/com/example/consumer/ConsumerServiceApplicationTests.java
@@ -1,11 +1,12 @@
 package com.example.consumer;
 
+import com.example.consumer.Model.ConsumerModel;
+import com.example.consumer.Repository.ConsumerRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 @DataJpaTest
@@ -20,8 +21,8 @@ class ConsumerServiceApplicationTests {
 	public void addNewUser(){
 		ConsumerModel consumerModel = new ConsumerModel();
 		consumerModel.setPassword("123");
-		consumerModel.setUsername("admin_gate_A");
-		consumerModel.setAccessLevel("GATE_GROUP_KEY_A");
+		consumerModel.setUsername("admin_gate_C");
+		consumerModel.setAccessLevel("GATE_GROUP_KEY_C");
 
 		ConsumerModel savedUser = repo.save(consumerModel);
 		Assertions.assertThat(savedUser).isNotNull();
