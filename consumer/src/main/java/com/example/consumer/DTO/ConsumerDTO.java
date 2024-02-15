c8b99728cf40bd91d89aef362614820f8a4f2d7e
agkj <alger.goh99@gmail.com>
2024-02-15 20:23:52 +0800

    Added new ConsumerDTO class to segregate ConsumerModel Entity class

---
+++ consumer/src/main/java/com/example/consumer/DTO/ConsumerDTO.java
@@ -0,0 +1,19 @@
+package com.example.consumer.DTO;
+
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ConsumerDTO {
+
+    private Integer id;
+    private String username;
+    private String password;
+    private String accessLevel;
+}
