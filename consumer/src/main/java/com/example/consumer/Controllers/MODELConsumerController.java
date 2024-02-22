30fe459953b3d3ec5bee9a6a2c6ae336c2c7c63c
agkj <alger.goh99@gmail.com>
2024-02-22 16:35:21 +0800

    Renamed function from displayGates to showHome, added some comments

--- consumer/src/main/java/com/example/consumer/Controllers/MODELConsumerController.java
+++ consumer/src/main/java/com/example/consumer/Controllers/MODELConsumerController.java
@@ -1,14 +1,17 @@
 package com.example.consumer.Controllers;
 
 import com.example.consumer.DTO.ConsumerDTO;
+import com.example.consumer.DTO.LoginDTO;
 import com.example.consumer.Services.ConsumerService;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,8 +20,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MODELConsumerController {
 
     private final ConsumerService consumerService;
+
+
+    //TODO: ADD FUNCTION TO CHECK LOGGED IN STATUS OF USER AND GRANT CERTAIN VIEW
     @GetMapping(path = "/home")
-    public String displayGates(Model model){
+    public String showHome(Model model){
+
         consumerService.displayGates(model);
         return "consumer-home";
     }
